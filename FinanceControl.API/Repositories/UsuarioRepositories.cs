9696093f076702058e215fc10e6c61314d42733f
Wanessa :) <wanessatlsousa@gmail.com>
2026-03-05 23:39:33 -0300

    atualizando repositorio de usuario

--- FinanceControl.API/Repositories/UsuarioRepositories.cs
+++ FinanceControl.API/Repositories/UsuarioRepositories.cs
@@ -3,14 +3,44 @@ using FinanceControl.API.Entities;
 
 namespace FinanceControl.API.Repositories
 {
-    public class UsuarioRepositories(DataContext context) : IUsuarioRepositories
+    public class UsuarioRepositories : IUsuarioRepositories
     {
-       protected IQueryable<Usuario> get => context.Set<Usuario>();
-       protected IQueryable<Usuario> getList => context.Set<Usuario>();
+        private readonly DataContext _context;
+
+        public UsuarioRepositories(DataContext context)
+        {
+            _context = context;
+        }
+
+        protected IQueryable<Usuario> get => _context.Set<Usuario>();
+
+        public IQueryable<Usuario> GetById(int id)
+        {
+            return get.Where(u => u.id == id);
+        }
+
+        public IQueryable<Usuario> GetByEmail(string email)
+        {
+            return get.Where(u => u.email == email);
+        }
+
+        public void InsertUsuario(Usuario entity)
+        {
+            _context.usuarios.Add(entity);
+        }
+
+        public void UpdateUsuario(Usuario entity)
+        {
+            _context.usuarios.Update(entity);
+        }
+
     }
 
     public interface IUsuarioRepositories
     {
-       
+        public IQueryable<Usuario> GetById(int id);
+        public IQueryable<Usuario> GetByEmail(string email);
+        public void InsertUsuario(Usuario entity);
+        public void UpdateUsuario(Usuario entity);
     }
 }
