142caad4760c562605e819e3e893debaf597fee9
Wanessa :) <wanessatlsousa@gmail.com>
2026-03-05 23:40:56 -0300

    criando controller de usuario e removendo controller de teste

---
+++ FinanceControl.API/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using FinanceControl.API.DTOs.Usuario;
+using FinanceControl.API.Entities;
+using FinanceControl.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceControl.API.Controllers
+{
+    [Route("Usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        [Route("/Usuario/{id}")]
+        public ActionResult<UsuarioResponse> ObterUsuarioPorId(int id)
+        {
+            return _usuarioService.BuscarPorId(id);
+        }
+
+        [HttpPost]
+        public ActionResult<UsuarioResponse> CriarUsuario(UsuarioRequest request)
+        {
+            return _usuarioService.CriarUsuario(request);
+        }
+    }
+}
