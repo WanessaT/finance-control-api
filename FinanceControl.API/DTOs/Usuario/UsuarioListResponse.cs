a2eda91dece68b38a77a14546ab8501fcbef1a27
Wanessa :) <wanessatlsousa@gmail.com>
2026-03-05 23:40:27 -0300

    atualizando dtos

--- FinanceControl.API/DTOs/Usuario/UsuarioListResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FinanceControl.API.DTOs.Usuario
-{
-    public class UsuarioListResponse : DTOBase
-    {
-        public string nome { get; set; }
-        public string email { get; set; }
-    }
-}
